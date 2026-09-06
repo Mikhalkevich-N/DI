b1c93aeb251e89392261c692a8c6b26efa3e3e48
MNV <snadd@yandex.ru>
2026-09-06 10:43:57 +0300

    Внесены изменения для задачи 3.3

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -5,11 +7,17 @@ from rest_framework.viewsets import ModelViewSet
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
 
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
-
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    
+    # Настройка фильтрации по дате и статусу
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['status', 'created_at']
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at']
+    
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated()]
         return []
